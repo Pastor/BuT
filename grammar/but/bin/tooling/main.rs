13fe832e0673602c5841e8c8871748c77bd0843e
Pastor <viruszold@gmail.com>
2023-12-19 00:29:04 +0300

    Model

--- grammar/but/bin/tooling/main.rs
+++ grammar/but/bin/tooling/main.rs
@@ -2,8 +2,13 @@ extern crate but;
 
 use std::fmt::Debug;
 use std::fs;
+use std::path::Path;
 
 use clap::Parser;
+use codespan_reporting::diagnostic::{Diagnostic, Label};
+use codespan_reporting::files::SimpleFile;
+use codespan_reporting::term;
+use codespan_reporting::term::termcolor::{ColorChoice, StandardStream};
 
 use but::helpers::ident::{DisplayIdent, FormatterIdent};
 
@@ -14,24 +19,34 @@ struct Args {
     source_file: String,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    let writer = StandardStream::stderr(ColorChoice::Always);
+    let config = term::Config::default();
     let args: Args = Args::parse();
     let path = args.source_file;
     let source = match fs::read_to_string(path.clone()) {
         Ok(source) => source,
         Err(err) => panic!("{}", err.to_string()),
     };
+    let path = Path::new(&path);
 
     let unit = match but::parse(&source, 0) {
         Ok(unit) => unit,
-        Err(errors) => panic!(
-            "{path:?}: \n\t{}",
-            errors
-                .iter()
-                .map(|diag| format!("{diag:?}"))
-                .collect::<Vec<_>>()
-                .join("\n\t")
-        ),
+        Err(errors) => {
+            let file = SimpleFile::new(path.file_name().unwrap().to_str().unwrap(), source);
+            for x in errors {
+                let message = x.message;
+                let diagnostic = Diagnostic::error()
+                    .with_message(message.clone())
+                    .with_labels(vec![Label::primary((), x.loc.start()..x.loc.end())
+                        .with_message(message.clone())])
+                    .with_notes(x.notes.iter().map(move |n| n.message.clone()).collect());
+
+                term::emit(&mut writer.lock(), &config, &file, &diagnostic)?;
+            }
+            panic!()
+        }
     };
     println!("{}", FormatterIdent::new(2, Box::new(unit.0)));
+    Ok(())
 }
