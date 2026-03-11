c9bfd1b1aec1b855bee601f2131ac458b652159b
Pastor <viruszold@gmail.com>
2026-03-11 23:45:44 +0300

    PrivateUnit

---
+++ .generate_synthesis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+./scripts/gen_all.sh grammar/tests_data/BuT/examples/synthesis \
+                     ./.temporary \
+                     --indent-size 2 \
+                     --include-dir grammar/tests_data/BuT/examples/synthesis
